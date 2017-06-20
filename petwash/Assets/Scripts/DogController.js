f0723424cf9c80d09d545d77e5d704ad5071fcc4
nickbrierley <nickcbrierley@gmail.com>
2017-06-19 21:44:29 -0700

    Dog spawns when game begins

---
+++ petwash/Assets/Scripts/DogController.js
@@ -0,0 +1,9 @@
+﻿#pragma strict
+var GermanShepard:GameObject;
+var SpawnPoint:GameObject;
+
+function Start() {
+    Instantiate(this.GermanShepard, this.SpawnPoint.transform.position, this.SpawnPoint.transform.rotation);
+}
+function Update () {
+}
