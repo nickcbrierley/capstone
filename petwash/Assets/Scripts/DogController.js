0c44f856f03778f95a1d9b0831350c474bb9ba77
nickbrierley <nickcbrierley@gmail.com>
2017-06-22 13:14:52 -0700

    Dog walks to counter

--- petwash/Assets/Scripts/DogController.js
+++
@@ -1,9 +0,0 @@
-﻿#pragma strict
-var GermanShepard:GameObject;
-var SpawnPoint:GameObject;
-
-function Start() {
-    Instantiate(this.GermanShepard, this.SpawnPoint.transform.position, this.SpawnPoint.transform.rotation);
-}
-function Update () {
-}
---
+++ petwash/Assets/Scripts/DogMovement.js
@@ -0,0 +1,12 @@
+﻿#pragma strict
+var target:Transform;
+var tub:Transform;
+
+function Start () {
+    var agent:UnityEngine.AI.NavMeshAgent = GetComponent.<UnityEngine.AI.NavMeshAgent>();
+    agent.destination = target.position;
+    agent.updateRotation = false;
+}
+
+function Update () {
+}
