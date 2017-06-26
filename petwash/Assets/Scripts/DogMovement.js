c35b177a65ccba49124d0e11fe4bd8e24bc94bd0
nickbrierley <nickcbrierley@gmail.com>
2017-06-26 11:23:53 -0700

    Dog goes to tub fron counter when clicked

--- petwash/Assets/Scripts/DogMovement.js
+++ petwash/Assets/Scripts/DogMovement.js
@@ -2,11 +2,21 @@
 var target:Transform;
 var tub:Transform;
 
+function OnMouseOver(){
+    var agent:UnityEngine.AI.NavMeshAgent = GetComponent.<UnityEngine.AI.NavMeshAgent>();
+    if(Vector3.Distance(transform.position, target.position) < 7.5f){
+        if(Input.GetMouseButtonDown(0)){
+            agent.destination = tub.GetComponent.<Renderer>().bounds.center;
+            print(Vector3.Distance(transform.position, target.position));
+        }
+    }
+}
+
 function Start () {
     var agent:UnityEngine.AI.NavMeshAgent = GetComponent.<UnityEngine.AI.NavMeshAgent>();
-    agent.destination = target.position;
-    agent.updateRotation = false;
+    agent.destination = target.GetComponent.<Renderer>().bounds.center;
 }
 
 function Update () {
+    OnMouseOver();
 }
