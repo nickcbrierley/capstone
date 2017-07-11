70d0035be22dca732258ebba22ffdbab388b93eb
nickbrierley <nickcbrierley@gmail.com>
2017-07-10 22:35:28 -0700

    Added the ability to create new tiles

---
+++ petwash/Assets/Scripts/CameraController.js
@@ -0,0 +1,17 @@
+﻿public var mouseSensitivity : float = 1.0;
+private var lastPosition : Vector3;
+ 
+function Update()
+{
+    if (Input.GetMouseButtonDown(0))
+    {
+        lastPosition = Input.mousePosition;
+    }
+ 
+    if (Input.GetMouseButton(0))
+    {
+        var delta : Vector3 = Input.mousePosition - lastPosition;
+        transform.Translate(delta.x * mouseSensitivity, delta.y * mouseSensitivity, 0);
+        lastPosition = Input.mousePosition;
+    }
+}
